5b5631d9f3dd6b274c2d5710900abc3f2c6c6e04
DiegoBulhoes <diegobulhoesm@gmail.com>
2019-06-07 20:39:15 -0400

    Corrige erros de conexão com DB

--- new_code/sisac/src/br/ufms/facom/sisac/Main.java
+++ new_code/sisac/src/br/ufms/facom/sisac/Main.java
@@ -2,13 +2,22 @@ package br.ufms.facom.sisac;
 
 import br.ufms.facom.sisac.dao.AlunoDAO;
 import br.ufms.facom.sisac.dao.PagamentoDAO;
+import br.ufms.facom.sisac.models.Aluno;
 import br.ufms.facom.sisac.models.RelatorioFinanceiro;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         AlunoDAO alunoDao = new AlunoDAO();
         PagamentoDAO pagamentoDao = new PagamentoDAO();
         
+        Aluno a1 = new Aluno();
         
-    }
+        a1.setNome("João");
+        a1.setEndereco("Rua X, 200 - Campo Grande/ MS");
+        a1.setDataMatricula(LocalDate.now());
+        a1.setDataLimiteMatricula(LocalDate.now());
+        
+        alunoDao.addAluno(a1);
+    }      
 }
