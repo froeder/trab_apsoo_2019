c01484c6482085a196f8579f2e6e03632f6257c4
DiegoBulhoes <diegobulhoesm@gmail.com>
2019-06-07 20:04:22 -0400

    update

--- new_code/sisac/src/br/ufms/facom/sisac/Main.java
+++ new_code/sisac/src/br/ufms/facom/sisac/Main.java
@@ -1,9 +1,14 @@
 package br.ufms.facom.sisac;
 
+import br.ufms.facom.sisac.dao.AlunoDAO;
+import br.ufms.facom.sisac.dao.PagamentoDAO;
 import br.ufms.facom.sisac.models.RelatorioFinanceiro;
 
 public class Main {
     public static void main(String[] args) {
+        AlunoDAO alunoDao = new AlunoDAO();
+        PagamentoDAO pagamentoDao = new PagamentoDAO();
+        
         
     }
 }
